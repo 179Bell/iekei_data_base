03c9f204031342a19f535e0b66262b91a3aec6da
179Bell <shingo.msdf@gmail.com>
2023-01-29 22:04:43 +0900

    Laravel uiを追加

---
+++ api/app/Http/Middleware/CheckAdmin.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::user()->admin !== true) {
+            return redirect()->route('/');
+        }
+
+        return $next($request);
+    }
+}
