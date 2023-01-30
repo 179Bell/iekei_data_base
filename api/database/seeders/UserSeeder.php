51543ff5c2eae109ab18f51185521d572c88d4bd
179Bell <shingo.msdf@gmail.com>
2023-01-30 21:46:25 +0900

    User,ShopInformationSeederを作成

---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => '管理者',
+            'email' => env('ADMIN_ADDRESS'),
+            'password' => Hash::make(env('ADMIN_PASSWORD')),
+            'admin' => true,
+        ]);
+    }
+}
